c24f97f42fd86285a690b57cbbf1914869609367
agus27dev <agus27.dev@gmail.com>
2021-05-02 07:51:58 +0700

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,93 @@
+<template>
+    <footer style="background: #e7ab3c;" class="pt-5 mt-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 mb-0 mt-0" style="color: #fff">
+                    <h4 class="font-weight-bold">TENTANG</h4>
+                    <hr style="border-top: 3px solid #fff;border-radius:.5rem">
+                    <p style="color: #fff">
+                        Online shop produk kewirausahaan SMK. Diproduksi oleh tangan terampil putra dan putri bangsa.
+                        Mari membangun negeri bersama kami, SMK Bisa !
+                    </p>
+                    <div class="social">
+                        <a href="#" class="mr-4" style="color: #fff"> <i class="fab fa-facebook fa-2x"></i></a>
+                        <a href="#" class="mr-4" style="color: #fff"> <i class="fab fa-twitter fa-2x"></i></a>
+                        <a href="#" class="mr-4" style="color: #fff;"> <i class="fab fa-instagram fa-2x"></i></a>
+                        <a href="#" class="mr-4" style="color: #fff;"> <i class="fab fa-youtube fa-2x"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4" style="color: #fff">
+                    <h4 class="font-weight-bold">PEMBAYARAAN</h4>
+                    <hr style="border-top: 3px solid #fff;border-radius:.5rem">
+                    <div class="row">
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card h-100 border-0 rounded shadow">
+                                <div class="card-body p-2 text-center">
+                                    <img src="@/assets/payment/BCA.png" style="width: 50px;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card h-100 border-0 rounded shadow">
+                                <div class="card-body p-2 text-center">
+                                    <img src="@/assets/payment/BNI.png" style="width: 45px;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card h-100 border-0 rounded shadow">
+                                <div class="card-body p-2 text-center">
+                                    <img src="@/assets/payment/BRI.png" style="width: 60px;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card h-100 border-0 rounded shadow">
+                                <div class="card-body p-2 text-center">
+                                    <img src="@/assets/payment/GOPAY.png" style="width: 60px;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card h-100 border-0 rounded shadow">
+                                <div class="card-body p-2 text-center">
+                                    <img src="@/assets/payment/indomaret-logo.png" style="width: 60px;">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-4 mb-3">
+                            <div class="card h-100 border-0 rounded shadow">
+                                <div class="card-body p-2 text-center">
+                                    <img src="@/assets/payment/atm-bersama.jpg" style="width: 40px;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4" style="color: #fff">
+                    <h4 class="font-weight-bold">INFO</h4>
+                    <hr style="border-top: 3px solid #fff;border-radius:.5rem">
+                    
+                        <i class="fa fa-clock" style="display:flex"><p style="padding-left: 10px; font-weight: normal;">Jam Operasional : 07.00 - 17.00 WIB</p></i> 
+                                            
+                        <i class="fa fa-map-marker" style="display:flex">
+                            <p style="padding-left: 10px; font-weight: normal;"> Jl. A.M. Sangaji No.47, Cokrodiningratan, Kec. Jetis, Kota Yogyakarta, Daerah Istimewa Yogyakarta</p>
+                        </i> 
+                    
+                </div>
+            </div>
+            <div class="row text-center mt-3 pb-3" style="color: #fff">
+                <div class="col-md-12">
+                    <hr>
+                    © <strong>Kreska</strong> 2021 • Kreasi Kewirausahaan SMK
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,38 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// import router dari folder router
+import router from './router'
+
+//import store vuex
+import store from './store'
+
+const app = createApp(App)
+
+// menggunkan router dengan plugin use
+app.use(router)
+
+//menggunakan store dengan plugin "use"
+app.use(store)
+
+// define mixin untuk global function atau helper
+app.mixin({
+    methods: {
+
+        //money thousands
+        moneyFormat(number) {
+            let reverse = number.toString().split('').reverse().join(''),
+            thousands   = reverse.match(/\d{1,3}/g)
+            thousands   = thousands.join('.').split('').reverse().join('')
+            return thousands
+        },
+
+        //calculate discount
+        calculateDiscount(product) {
+            return product.price - (product.price *(product.discount)/100)
+        }
+
+    }
+})
+
+app.mount('#app')
